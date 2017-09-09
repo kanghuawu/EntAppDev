5840c37f8bd0a46e60c1249259da37acb15471c1
kanghuawu <kang-hua.wu@sjsu.edu>
2017-09-08 21:46:30 -0700

    change name

--- HelloWorld/src/main/java/com/kanghua/helloworld/HelloworldApplication.java
+++ HelloWorld/src/main/java/com/kanghua/helloworld/HelloworldApplication.java
@@ -4,7 +4,7 @@ package com.kanghua.helloworld;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class HelloworldApplication {
+public class HelloWorldApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
